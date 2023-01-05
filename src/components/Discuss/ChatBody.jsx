213d0ff89dae90ab104877ebb15a8efcf41cbf1f
thagoo <lohitgowda56@gmail.com>
2023-01-05 20:49:55 +0530

    Chat: Add message bubbles and get ready for admin controls
    Signed-off-by: thagoo <lohitgowda56@gmail.com>

--- src/components/Discuss/ChatBody.jsx
+++ src/components/Discuss/ChatBody.jsx
@@ -10,7 +10,13 @@ import {
   Divider,
   Snackbar,
   IconButton,
+  Popover,
+  MenuItem,
+  ListItemIcon,
+  Avatar,
 } from "@material-ui/core/";
+import DeleteIcon from "@mui/icons-material/Delete";
+import RemoveCircleRoundedIcon from "@mui/icons-material/RemoveCircleRounded";
 import CloseButton from "@mui/icons-material/Close";
 import { useEffect } from "react";
 
@@ -20,6 +26,15 @@ const ChatBody = ({ username, messages, socket, room, lastMessageRef }) => {
   const handleClose = () => {
     setOpen(false);
   };
+
+  const [optionShow, setOptionShow] = useState(null);
+  const handleOptionShow = (event) => setOptionShow(event.currentTarget);
+  const handleOptionClose = () => setOptionShow(null);
+  const show = Boolean(optionShow);
+  const msgOption = open ? "simple-popover" : undefined;
+
+  const handleMute = () => {};
+  const handleDeleteMsg = () => {};
   const closeSnackBar = (
     <IconButton
       size="small"
@@ -68,25 +83,68 @@ const ChatBody = ({ username, messages, socket, room, lastMessageRef }) => {
               How about a discussion on A.I or a popular person like Elon Musk?
             </p>
           </Alert>
+
           {messages.map((message) => (
             <ListItem key="1">
               <Grid container>
-                <Grid item xs={12}>
-                  <ListItemText
-                    secondary={
-                      message.username == username ? "You" : message.username
-                    }
-                    align={message.username == username ? "right" : "left"}
-                  ></ListItemText>
-                  <ListItemText
-                    align={message.username == username ? "right" : "left"}
-                    primary={message.message}
-                  ></ListItemText>
+                <Grid
+                  item
+                  xs={11}
+                  style={{ display: `flex`, flexDirection: `column` }}
+                >
+                  <Popover
+                    id={msgOption}
+                    open={show}
+                    onClose={handleOptionClose}
+                    anchorEl={optionShow}
+                    anchorOrigin={{
+                      vertical: "top",
+                      horizontal: "center",
+                    }}
+                    transformOrigin={{
+                      vertical: "center",
+                      horizontal: "center",
+                    }}
+                  >
+                    <MenuItem onClick={handleDeleteMsg}>
+                      <DeleteIcon style={{ marginRight: `6px` }} />
+                      Delete
+                    </MenuItem>
+                    <MenuItem onClick={handleMute}>
+                      <RemoveCircleRoundedIcon style={{ marginRight: `6px` }} />
+                      Mute User
+                    </MenuItem>
+                  </Popover>
+
+                  <List
+                    button
+                    key="1"
+                    className="msg-bubble"
+                    style={{
+                      alignSelf: `${
+                        message.username == username ? "flex-end" : "flex-start"
+                      }`,
+                    }}
+                  >
+                    {message.username == username ? null : (
+                      <ListItem style={{ paddingLeft: 5 }}>
+                        <Avatar
+                          style={{ width: 22, height: 22, marginRight: 10 }}
+                        ></Avatar>
+                        <ListItemText secondary={message.username} />
+                      </ListItem>
+                    )}
+                    <ListItem button onClick={handleOptionShow}>
+                      <ListItemText
+                        primary={message.message}
+                        secondary={message.time}
+                      />
+                    </ListItem>
+                  </List>
                 </Grid>
-                <Grid item xs={12}>
+                <Grid item xs={11}>
                   <ListItemText
                     align={message.username == username ? "right" : "left"}
-                    secondary={message.time}
                   ></ListItemText>
                 </Grid>
               </Grid>
